552b4520c31dea6b625d2318fe13d81c5709c95c
gorden5566 <gorden5566@126.com>
2019-12-09 00:53:50 +0800

    工具类

--- apollo-core/src/main/java/com/ctrip/framework/foundation/internals/Utils.java
+++ apollo-core/src/main/java/com/ctrip/framework/foundation/internals/Utils.java
@@ -2,11 +2,25 @@ package com.ctrip.framework.foundation.internals;
 
 import com.google.common.base.Strings;
 
+/**
+ * 工具类
+ */
 public class Utils {
+  /**
+   * 字符串是否为空
+   *
+   * @param str
+   * @return
+   */
   public static boolean isBlank(String str) {
     return Strings.nullToEmpty(str).trim().isEmpty();
   }
 
+  /**
+   * 判断是否为 windows 系统
+   *
+   * @return
+   */
   public static boolean isOSWindows() {
     String osName = System.getProperty("os.name");
     if (Utils.isBlank(osName)) {
