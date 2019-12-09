c452e8a2897091c768d8177a24fbfeb6bcbe18cf
gorden5566 <gorden5566@126.com>
2019-12-10 00:52:09 +0800

    ConfigFactory 工厂的默认实现类

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spi/DefaultConfigFactoryManager.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spi/DefaultConfigFactoryManager.java
@@ -9,8 +9,15 @@ import com.google.common.collect.Maps;
  * @author Jason Song(song_s@ctrip.com)
  */
 public class DefaultConfigFactoryManager implements ConfigFactoryManager {
+  /**
+   * ConfigFactory 注册中心，用于管理 namespace 对应的 ConfigFactory
+   */
   private ConfigRegistry m_registry;
 
+  /**
+   * key: namespace
+   * value: ConfigFactory 实例
+   */
   private Map<String, ConfigFactory> m_factories = Maps.newConcurrentMap();
 
   public DefaultConfigFactoryManager() {
@@ -20,6 +27,7 @@ public class DefaultConfigFactoryManager implements ConfigFactoryManager {
   @Override
   public ConfigFactory getFactory(String namespace) {
     // step 1: check hacked factory
+    // 先到注册中心取
     ConfigFactory factory = m_registry.getFactory(namespace);
 
     if (factory != null) {
@@ -27,6 +35,7 @@ public class DefaultConfigFactoryManager implements ConfigFactoryManager {
     }
 
     // step 2: check cache
+    // 再到缓存取
     factory = m_factories.get(namespace);
 
     if (factory != null) {
@@ -34,6 +43,7 @@ public class DefaultConfigFactoryManager implements ConfigFactoryManager {
     }
 
     // step 3: check declared config factory
+    // 到guice容器取，目前未实现
     factory = ApolloInjector.getInstance(ConfigFactory.class, namespace);
 
     if (factory != null) {
@@ -41,6 +51,7 @@ public class DefaultConfigFactoryManager implements ConfigFactoryManager {
     }
 
     // step 4: check default config factory
+    // 到guice容器取
     factory = ApolloInjector.getInstance(ConfigFactory.class);
 
     m_factories.put(namespace, factory);
