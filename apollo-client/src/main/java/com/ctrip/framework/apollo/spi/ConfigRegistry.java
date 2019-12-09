0124545927354530f4534c82dcf966577106c820
gorden5566 <gorden5566@126.com>
2019-12-10 00:46:16 +0800

    ConfigFactory 注册中心接口

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spi/ConfigRegistry.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spi/ConfigRegistry.java
@@ -1,6 +1,8 @@
 package com.ctrip.framework.apollo.spi;
 
 /**
+ * ConfigFactory 注册中心接口
+ *
  * The manually config registry, use with caution!
  *
  * @author Jason Song(song_s@ctrip.com)
@@ -12,7 +14,7 @@ public interface ConfigRegistry {
    * @param namespace the namespace
    * @param factory   the factory for this namespace
    */
-  public void register(String namespace, ConfigFactory factory);
+  void register(String namespace, ConfigFactory factory);
 
   /**
    * Get the registered config factory for the namespace.
@@ -20,5 +22,5 @@ public interface ConfigRegistry {
    * @param namespace the namespace
    * @return the factory registered for this namespace
    */
-  public ConfigFactory getFactory(String namespace);
+  ConfigFactory getFactory(String namespace);
 }
