ea0591910b78bd40992457c0a832679603b4893a
gorden5566 <gorden5566@126.com>
2019-12-15 22:48:55 +0800

    ConfigPropertySourceFactory

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spring/config/ConfigPropertySourceFactory.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spring/config/ConfigPropertySourceFactory.java
@@ -5,18 +5,35 @@ import java.util.List;
 import com.ctrip.framework.apollo.Config;
 import com.google.common.collect.Lists;
 
+/**
+ * ConfigPropertySource factory
+ */
 public class ConfigPropertySourceFactory {
 
   private final List<ConfigPropertySource> configPropertySources = Lists.newLinkedList();
 
+  /**
+   * 获取 ConfigPropertySource
+   *
+   * @param name
+   * @param source
+   * @return
+   */
   public ConfigPropertySource getConfigPropertySource(String name, Config source) {
+    // 创建 ConfigPropertySource
     ConfigPropertySource configPropertySource = new ConfigPropertySource(name, source);
 
+    // 保存下来
     configPropertySources.add(configPropertySource);
 
     return configPropertySource;
   }
 
+  /**
+   * 获取所有的 ConfigPropertySource
+   *
+   * @return
+   */
   public List<ConfigPropertySource> getAllConfigPropertySources() {
     return Lists.newLinkedList(configPropertySources);
   }
